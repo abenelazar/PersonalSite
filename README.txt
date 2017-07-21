c3bfbafc80ccf207e867bd00ed750edb41ea4037
Ari Ben-Elazar <abenelazar@gmail.com>
2017-07-20 23:45:28 -0400

    new site

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/owOeby.
+
+ 'Opening sequence' text effect.
---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+      		e.preventDefault();
+      		$('body').toggleClass('nav-expanded');
+      	});
+      	$('#nav-close').on('click',function(e){
+      		e.preventDefault();
+      		$('body').removeClass('nav-expanded');
+      	});
+
+      	// Initialize navgoco with default options
+        $(".main-menu").navgoco({
+            caret: '<span class="caret"></span>',
+            accordion: false,
+            openClass: 'open',
+            save: true,
+            cookie: {
+                name: 'navgoco',
+                expires: false,
+                path: '/'
+            },
+            slide: {
+                duration: 300,
+                easing: 'swing'
+            }
+        });
+
+      });
+
+$("#os-phrases > h2")
+    .css('opacity', 1).lettering( 'words' )
+    .children( "span" ).lettering()
+    .children( "span" ).lettering();
+
+
+
